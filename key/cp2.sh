519649496dddd4f26de437515ee92a2ac2dd2f8d
vewe-richard <jiang.j.qian@qq.com>
2022-09-28 10:31:12 +0000

    mkdir -p

--- key/cp2.sh
+++ key/cp2.sh
@@ -1,11 +1,11 @@
-mkdir /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
-mkdir /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
-mkdir /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
-mkdir /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
-mkdir /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
-mkdir /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
-mkdir /etc/letsencrypt/live/ap-demo-core.labflow.ai/
-mkdir /etc/letsencrypt/live/ap-demo-core.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-core.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-demo-core.labflow.ai/
 
 cp fullchain.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
 cp privkey.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
