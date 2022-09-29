10f0ab687b690792dc38f89f89359638ae34151f
vewe-richard <jiang.j.qian@qq.com>
2022-09-29 02:41:46 +0000

    update cp2.sh

--- key/cp2.sh
+++ key/cp2.sh
@@ -1,11 +1,14 @@
-cp fullchain.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
-cp privkey.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
-cp fullchain.pem /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
-cp privkey.pem /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
-cp fullchain.pem /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
-cp privkey.pem /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
-cp fullchain.pem /etc/letsencrypt/live/ap-demo-core.labflow.ai/
-cp privkey.pem /etc/letsencrypt/live/ap-demo-core.labflow.ai/
+mkdir -p /etc/letsencrypt/live/localhost-upload/
+mkdir -p /etc/letsencrypt/live/localhost-ims2/
+mkdir -p /etc/letsencrypt/live/localhost-ims/
+mkdir -p /etc/letsencrypt/live/localhost-core/
 
-
+cp fullchain.pem /etc/letsencrypt/live/localhost-upload/
+cp privkey.pem /etc/letsencrypt/live/localhost-upload/
+cp fullchain.pem /etc/letsencrypt/live/localhost-ims2/
+cp privkey.pem /etc/letsencrypt/live/localhost-ims2/
+cp fullchain.pem /etc/letsencrypt/live/localhost-ims/
+cp privkey.pem /etc/letsencrypt/live/localhost-ims/
+cp fullchain.pem /etc/letsencrypt/live/localhost-core/
+cp privkey.pem /etc/letsencrypt/live/localhost-core/
 
