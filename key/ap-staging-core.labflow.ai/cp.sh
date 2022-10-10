13df4fcde942278606a69684cb8be459197bc20b
vewe-richard <jiang.j.qian@qq.com>
2022-10-10 07:51:48 +0000

    remove generated files

--- key/ap-staging-core.labflow.ai/cp.sh
+++ key/ap-staging-core.labflow.ai/cp.sh
@@ -3,7 +3,7 @@ mkdir -p /etc/letsencrypt/live/ap-staging-ims2.labflow.ai/
 mkdir -p /etc/letsencrypt/live/ap-staging-ims.labflow.ai/
 mkdir -p /etc/letsencrypt/live/ap-staging-core.labflow.ai/
 
-cp -v key/ap-staging-core.labflow.ai/fullchain.pem /etc/letsencrypt/live/ap-staging-upload.labflow.ai/
+cp key/ap-staging-core.labflow.ai/fullchain.pem /etc/letsencrypt/live/ap-staging-upload.labflow.ai/
 cp key/ap-staging-core.labflow.ai/privkey.pem /etc/letsencrypt/live/ap-staging-upload.labflow.ai/
 cp key/ap-staging-core.labflow.ai/fullchain.pem /etc/letsencrypt/live/ap-staging-ims2.labflow.ai/
 cp key/ap-staging-core.labflow.ai/privkey.pem /etc/letsencrypt/live/ap-staging-ims2.labflow.ai/
