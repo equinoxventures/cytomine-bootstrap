e47ac7316003e9269c8175acf15faf6f0f7ea7f8
vewe-richard <jiang.j.qian@qq.com>
2022-10-13 07:37:32 +0000

    add keys for demo site

--- key/ap-demo-core.labflow.ai/cp.sh
+++ key/ap-demo-core.labflow.ai/cp.sh
@@ -3,12 +3,12 @@ mkdir -p /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
 mkdir -p /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
 mkdir -p /etc/letsencrypt/live/ap-demo-core.labflow.ai/
 
-cp key/ap-demo-core.labflow.ai/fullchain.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
-cp key/ap-demo-core.labflow.ai/privkey.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
-cp key/ap-demo-core.labflow.ai/fullchain.pem /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
-cp key/ap-demo-core.labflow.ai/privkey.pem /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
-cp key/ap-demo-core.labflow.ai/fullchain.pem /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
-cp key/ap-demo-core.labflow.ai/privkey.pem /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
-cp key/ap-demo-core.labflow.ai/fullchain.pem /etc/letsencrypt/live/ap-demo-core.labflow.ai/
-cp key/ap-demo-core.labflow.ai/privkey.pem /etc/letsencrypt/live/ap-demo-core.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-upload.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-upload.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-demo-upload.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-ims2.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-ims2.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-demo-ims2.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-ims.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-ims.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-demo-ims.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-core.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-demo-core.labflow.ai/
+cp key/ap-demo-core.labflow.ai/archive/ap-demo-core.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-demo-core.labflow.ai/
 
