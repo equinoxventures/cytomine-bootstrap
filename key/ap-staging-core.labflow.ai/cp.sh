ec54db6eaa1bc71ab7ebccc9465451c97570560d
Ubuntu <vewe-richard>
2022-10-20 05:54:50 +0000

    add keys

---
+++ key/ap-staging-core.labflow.ai/cp.sh
@@ -0,0 +1,14 @@
+mkdir -p /etc/letsencrypt/live/ap-staging-upload.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-staging-ims2.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-staging-ims.labflow.ai/
+mkdir -p /etc/letsencrypt/live/ap-staging-core.labflow.ai/
+
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-upload.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-staging-upload.labflow.ai/
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-upload.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-staging-upload.labflow.ai/
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-ims2.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-staging-ims2.labflow.ai/
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-ims2.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-staging-ims2.labflow.ai/
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-ims.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-staging-ims.labflow.ai/
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-ims.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-staging-ims.labflow.ai/
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-core.labflow.ai/fullchain1.pem /etc/letsencrypt/live/ap-staging-core.labflow.ai/
+cp key/ap-staging-core.labflow.ai/archive/ap-staging-core.labflow.ai/privkey1.pem /etc/letsencrypt/live/ap-staging-core.labflow.ai/
+
