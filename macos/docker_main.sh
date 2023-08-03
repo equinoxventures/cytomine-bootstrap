8d5a1fa6dbc300a115b86b3988a4320f1597d300
joon <joon@eqx.vc>
2023-08-03 17:10:42 +1000

    minor

--- macos/docker_main.sh
+++ macos/docker_main.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# cp macos/configuration.sh configuration.sh
-
 bash init.sh
--- macos/init_using_docker.sh
+++ macos/init_using_docker.sh
@@ -1,11 +1,4 @@
-# Build the Docker image
-# docker build -t cytomine-bootstrap \
-#     -t ./macos/Dockerfile
-
-# echo " > backing up config.sh and init.sh..."
-# mkdir backup
-# cp configuration.sh backup/
-# cp init.sh backup/
+#!/bin/bash
 
 # Run the Docker container
 docker run -it --rm \
@@ -13,7 +6,3 @@ docker run -it --rm \
     -w /app \
     --entrypoint /app/macos/docker_main.sh \
     ubuntu:latest
-
-# echo " > restoring config.sh and init.sh..."
-# cp backup/configuration.sh .
-# cp backup/init.sh .
