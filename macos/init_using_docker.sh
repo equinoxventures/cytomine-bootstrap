677d7ae0048766f2d0d8f01d7e01cfb829b29d28
joon <joon@eqx.vc>
2023-08-04 10:39:51 +1000

    data_dir

--- macos/init_using_docker.sh
+++ macos/init_using_docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+export DATA_DIR=/Users/joon/
+
+. ./configuration.sh >/dev/null 2>&1
+mkdir -p $IMS_STORAGE_PATH
+mkdir -p $IMS_BUFFER_PATH
+mkdir -p $UPLOADED_SOFTWARES_PATH
+
 # Run the Docker container
 docker run -it --rm \
     -v $PWD:/app \
     -w /app \
+    -e DATA_DIR=$DATA_DIR \
     --entrypoint /app/macos/docker_main.sh \
     ubuntu:latest
